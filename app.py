4b72f911ce8fd5e7fa671ca1e5aa5ab617fd0d3d
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-10-10 14:09:40 +0900

    お試し

--- app.py
+++ app.py
@@ -1,29 +1,17 @@
-# from flask import Flask
-# from flask import render_template, request
-# import os
-# from flask_cors import CORS
-
-# app = Flask(__name__)
-# CORS(app)
-
-# @app.route('/upload', methods=['GET', 'POST'])
-# def upload_file():
-#   if request.method == 'POST':
-#     file = request.files['file'] # type: ignore
-#     file.save(os.path.join('./static/image', file.filename))
-#     return f'{file.filename}がアップロードされました'
-#   else:
-#     return render_template('upload.html')
-
-# if __name__ == '__main__':
-#   app.run(host='0.0.0.0', port=5000)
-
 from flask import Flask
+from flask import render_template, request
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Web App with Python Flask!'
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+  if request.method == 'POST':
+    file = request.files['file'] # type: ignore
+    file.save(os.path.join('./static/image', file.filename))
+    return f'{file.filename}がアップロードされました'
+  else:
+    return render_template('upload.html')
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+  app.run(debug=True)
