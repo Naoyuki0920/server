0689308319e7b6816703fc6025db60fc61c31bc1
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-10-10 14:17:12 +0900

    オプション追加

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template, request
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="template")
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -14,4 +14,4 @@ def upload_file():
     return render_template('upload.html')
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(host='0.0.0.0', port=5000)
