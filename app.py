f4d7bac17c19b3a2cb7a653c015e9a0c7ebbb80b
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-10-10 15:44:26 +0900

    ディレクトリ動的変更

--- app.py
+++ app.py
@@ -4,14 +4,20 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-  if request.method == 'POST':
-    file = request.files['file'] # type: ignore
-    file.save(os.path.join('./static/image', file.filename))
-    return render_template('finish.html', filename=file.filename)
-  else:
-    return render_template('upload.html')
+    if request.method == 'POST':
+        number = request.form['number']  # type: ignore
+        file = request.files['file']  # type: ignore
+        folder_path = os.path.join('./static/image', number)
+        if not os.path.exists(folder_path):
+            os.makedirs('./static/image')
+        file.save(os.path.join(folder_path, file.filename))
+        return render_template('finish.html', filename=file.filename)
+    else:
+        return render_template('upload.html')
+
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
