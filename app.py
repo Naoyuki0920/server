db903cefa6cd87f463162f6850512bafbf1333e7
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-10-10 14:02:04 +0900

    お試し

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
+# from flask import Flask
+# from flask import render_template, request
+# import os
+# from flask_cors import CORS
+
+# app = Flask(__name__)
+# CORS(app)
+
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload_file():
+#   if request.method == 'POST':
+#     file = request.files['file'] # type: ignore
+#     file.save(os.path.join('./static/image', file.filename))
+#     return f'{file.filename}がアップロードされました'
+#   else:
+#     return render_template('upload.html')
+
+# if __name__ == '__main__':
+#   app.run(host='0.0.0.0', port=5000)
+
 from flask import Flask
-from flask import render_template, request
-import os
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-  if request.method == 'POST':
-    file = request.files['file'] # type: ignore
-    file.save(os.path.join('./static/image', file.filename))
-    return f'{file.filename}がアップロードされました'
-  else:
-    return render_template('upload.html')
+@app.route('/')
+def index():
+    return 'Web App with Python Flask!'
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+app.run(host='0.0.0.0', port=5000)
