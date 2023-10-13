aad86492481b8f021af0d912c1cfdd77719833f6
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-10-13 15:21:36 +0900

    ソケット変更

--- send.py
+++ send.py
@@ -3,6 +3,7 @@ import socket
 import schedule
 from time import sleep
 
+
 def send_glb(glb_path, ip_addr, port):
     with open(glb_path, "rb") as f:
         glb_data = f.read()
@@ -13,12 +14,13 @@ def send_glb(glb_path, ip_addr, port):
     sock.sendall(glb_data)
     sock.close()
 
+
 if __name__ == "__main__":
     glb_path = "./static/image/1/Astronaut.glb"
-    ip_addr = "192.168.75.131"
+    ip_addr = "192.168.0.11"
     port = 5000
     schedule.every(120).seconds.do(send_glb(glb_path, ip_addr, port))
 
     while True:
-      schedule.run_pending()
-      sleep(1)
+        schedule.run_pending()
+        sleep(1)
